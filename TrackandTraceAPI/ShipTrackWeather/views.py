7ccd7880be1c4877773ea885b29a16ec72361082
imanrd <i.rezayan@gmail.com>
2024-01-05 18:51:39 +0330

    Add weather view

--- TrackandTraceAPI/ShipTrackWeather/views.py
+++ TrackandTraceAPI/ShipTrackWeather/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from configparser import ConfigParser
+import requests
 
-# Create your views here.
+parser = ConfigParser()
+parser.read("environment.ini")
+base_url = parser["API"]["base_url"]
+units = parser["API"]["units"]
+APPID = parser["API"]["APPID"]
+
+
+def get_weather_data(location):
+    cached_weather_data = cache.get(location)
+
+    if cached_weather_data:
+        # If data exists in cache, return cached data
+        return cached_weather_data
+    else:
+        # If not in cache, fetch data from the weather API
+        weather_api_url = f"{base_url}?q={location}&units={units}&APPID={APPID}"
+        response = requests.get(weather_api_url)
+
+        if response.status_code == 200:
+            weather_data = response.json()
+            cache.set(location, weather_data, timeout=7200)
+            return weather_data
+        else:
+            return None
