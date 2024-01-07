5ad7f8250f0391003775ee15493c3bb8d4838516
imanrd <i.rezayan@gmail.com>
2024-01-07 10:06:19 +0330

    Change views to use WeatherService

--- TrackandTraceAPI/ShipTrackWeather/views.py
+++ TrackandTraceAPI/ShipTrackWeather/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from .weather_utils import get_weather_data
+from .weather_utils import WeatherService
 from .queries import get_shipment_by_tracking_number_and_carrier
 from ShipTrackWeather import logger
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 def get_receiver_city(shipments):
@@ -27,6 +30,34 @@ class WeatherShipmentView(View):
         logger.info("GET request received")
         return self.render_template(request)
 
+    @method_decorator(name='post', decorator=swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'tracking_number': openapi.Schema(type=openapi.TYPE_STRING),
+                'carrier': openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=['tracking_number', 'carrier']
+        ),
+        responses={
+            200: openapi.Response(
+                description="Successful retrieval of weather and shipment data",
+                examples={
+                    'weather_data':
+                        """
+                        Weather Information for Paris
+                        Temperature: 7.14°C
+                        Weather: overcast clouds""",
+                    'shipments':
+                    """
+                    Shipment Details
+                    Shipment: TN12345678 - DHL - in-transit
+                    Shipment: TN12345678 - DHL - in-transit
+                    """
+                }
+            ),
+        }
+    ))
     def post(self, request):
         logger.info("Post request received")
         tracking_number = request.POST.get('tracking_number')
@@ -34,7 +65,7 @@ class WeatherShipmentView(View):
 
         shipments = get_shipment_by_tracking_number_and_carrier(tracking_number, carrier)
         location = get_receiver_city(shipments)
-        weather_data = get_weather_data(location)
+        weather_data = WeatherService.get_weather_data(location)
 
         context = {
 
