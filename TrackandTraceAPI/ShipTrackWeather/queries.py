71b79a63c78534e3c4a2cf0d6602338ffd037ffa
imanrd <i.rezayan@gmail.com>
2024-01-08 11:30:44 +0330

    Added comprehensive docstrings and type annotations for improved readability and clarity in queries.py.

--- TrackandTraceAPI/ShipTrackWeather/queries.py
+++ TrackandTraceAPI/ShipTrackWeather/queries.py
@@ -1,7 +1,18 @@
 from .models import Shipment
+from typing import Optional
 
 
-def get_shipment_by_tracking_number_and_carrier(tracking_number, carrier):
+def get_shipment_by_tracking_number_and_carrier(tracking_number: str, carrier: str) -> Optional[Shipment]:
+    """
+    Retrieves shipments based on tracking number and carrier, with associated article names.
+
+    Args:
+    - tracking_number (str): The tracking number to search for.
+    - carrier (str): The carrier to search for.
+
+    Returns:
+    - Shipment or None: QuerySet of shipments if found, else None.
+    """
     try:
         shipments = Shipment.objects.filter(tracking_number=tracking_number, carrier=carrier)
         for shipment in shipments:
