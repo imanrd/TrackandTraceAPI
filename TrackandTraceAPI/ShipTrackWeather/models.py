936264987c3b10439fbc7cf6cf38319c01bdf50a
imanrd <i.rezayan@gmail.com>
2024-01-08 11:00:41 +0330

    Added comprehensive docstrings and type annotations for improved readability and clarity in models.py.

--- TrackandTraceAPI/ShipTrackWeather/models.py
+++ TrackandTraceAPI/ShipTrackWeather/models.py
@@ -1,26 +1,49 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class Shipment(models.Model):
-    tracking_number = models.CharField(max_length=100)
-    carrier = models.CharField(max_length=100)
-    sender_address = models.TextField()
-    receiver_address = models.TextField()
-    status = models.CharField(max_length=50)
+    """
+    Model representing a shipment.
 
-    def __str__(self):
+    Attributes:
+    - tracking_number (CharField): Tracking number for the shipment.
+    - carrier (CharField): Carrier information for the shipment.
+    - sender_address (TextField): Address of the sender.
+    - receiver_address (TextField): Address of the receiver.
+    - status (CharField): Status of the shipment.
+    """
+    tracking_number: str = models.CharField(max_length=100)
+    carrier: str = models.CharField(max_length=100)
+    sender_address: str = models.TextField()
+    receiver_address: str = models.TextField()
+    status: str = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Shipment instance.
+        """
         return f"{self.tracking_number} - {self.carrier} - {self.status}"
 
 
 class Article(models.Model):
-    shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE)
-    article_name = models.CharField(max_length=100)
-    article_quantity = models.IntegerField()
-    article_price = models.DecimalField(max_digits=10, decimal_places=2)
-    SKU = models.CharField(max_length=50)
+    """
+    Model representing an article.
 
-    def __str__(self):
+    Attributes:
+    - shipment (ForeignKey): ForeignKey to Shipment model.
+    - article_name (CharField): Name of the article.
+    - article_quantity (IntegerField): Quantity of the article.
+    - article_price (DecimalField): Price of the article.
+    - SKU (CharField): Stock Keeping Unit of the article.
+    """
+    shipment: Shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE)
+    article_name: str = models.CharField(max_length=100)
+    article_quantity: str = models.IntegerField()
+    article_price: str = models.DecimalField(max_digits=10, decimal_places=2)
+    SKU: str = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Article instance.
+        """
         return f"{self.article_name} - {self.SKU}"
