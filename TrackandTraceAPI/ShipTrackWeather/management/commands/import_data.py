1588fed03309eab37eafdff4622f19f610acb699
imanrd <i.rezayan@gmail.com>
2024-01-08 11:10:53 +0330

    Added comprehensive docstrings and type annotations for improved readability and clarity in import_data.py.

--- TrackandTraceAPI/ShipTrackWeather/management/commands/import_data.py
+++ TrackandTraceAPI/ShipTrackWeather/management/commands/import_data.py
@@ -1,21 +1,26 @@
 import csv
 from django.core.management.base import BaseCommand
-import sys
-
-sys.path.append('/home/iman/Projects/TrackandTraceAPI/TrackandTraceAPI/')
 from ShipTrackWeather.models import Shipment, Article
 
 
 class Command(BaseCommand):
-    help = 'Import data from CSV file to database'
+    """
+    Django management command to import data from a CSV file to the database.
+    """
+    help: str = 'Import data from CSV file to database'
 
     def handle(self, *args, **options):
+        """
+        Handles the importing of data from the CSV file to the database.
+
+        Reads data from 'data.csv' and creates Shipment and Article objects in the database.
+        """
         file_path = 'data.csv'
 
         with open(file_path, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                shipment = Shipment.objects.create(
+                shipment: Shipment = Shipment.objects.create(
                     tracking_number=row['tracking_number'],
                     carrier=row['carrier'],
                     sender_address=row['sender_address'],
